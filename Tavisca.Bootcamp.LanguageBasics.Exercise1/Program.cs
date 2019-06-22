6ec4a01ce31a458bad837e8c2969bca8fe931c86
Raja Sachin <tavisca-rsachin>
2019-06-22 12:58:57 +0530

    First Commit

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,42 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            string[] arrABC = equation.Split("*");
+            string A = arrABC[0];
+            string[] arrBC =  arrABC[1].Split("=");
+            string B = arrBC[0];
+            string C = arrBC[1];
+
+            if(C.Contains('?'))
+                return CalValue(A, B, C, 3);
+            else if(A.Contains('?'))
+                return CalValue(C, B, A, 1);
+            else if(B.Contains('?'))
+                return CalValue(C, A, B, 2);
+            return -1;
+        }
+        public static int CalValue(string X, string Y, string Z, int i)
+        {
+            int x = Int32.Parse(X);
+            int y = Int32.Parse(Y);
+            
+            int z;
+            if(i == 3)
+                z = x * y;
+            else 
+            {
+                if(x == 0 || y == 0 || x % y != 0)
+                    return -1;
+                z = x / y;
+            }
+
+            string res = Convert.ToString(z);
+
+            if(res.Length != Z.Length)
+                    return -1;
+
+            int ind = Z.IndexOf('?');
+            return (int)(res[ind]-'0');
         }
     }
 }
